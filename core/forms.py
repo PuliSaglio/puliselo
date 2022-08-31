619c5bad436fea52576586a50afc6b541256bf69
Marcelo Delmiro <ifrnjoao@gmail.com>
2022-08-31 11:06:28 -0300

    alterei muita coisa mas ainda tá ruim (y)

--- core/forms.py
+++ core/forms.py
@@ -7,14 +7,14 @@ class CursoForm(ModelForm):
         model = Curso
         fields = ['titulo', 'vagas']
 
-    ''''''''
+
 
 class UsuarioForm(ModelForm):
     class Meta:
         model = Usuario
         fields = ['nome_user', 'cpf', 'email', 'foto']
 
-    ''''''''
+
 
 class InstituicaoForm(ModelForm):
     class Meta:
